e4e6b07a8e316f2f7a400ed2a9b312e0c181eea3
taemi <taemi@chungbuk.ac.kr>
2021-03-09 22:47:13 +0900

    v1_parsing for one video

--- parsing_LRGC_mnm.py
+++ parsing_LRGC_mnm.py
@@ -1,6 +1,7 @@
 import cv2
 from xml.etree.ElementTree import parse
 import sys
+import pickle
 
 
 def parsing_annotation(path, name):
@@ -25,7 +26,6 @@ def parsing_annotation(path, name):
         if x.attrib['label'] == 'pedestrian':
             new_annotations.append(x)
             
-
     # for track in new_annotations:
     #     for box in track:
             # print(box[0].text) # 0_1_2b
@@ -66,28 +66,90 @@ def parsing_appearance(path, name):
     return new_appearnaces
 
 def cut_img(img, f_num, box):
-    xbr = box.attrib['xbr']
-    xtl = box.attrib['xtl']
-    ybr = box.attrib['ybr']
-    ytl = box.attrib['ytl']
-
-    c_img = img[xtl:xbr, ytl:ybr]
+    
+    xbr = int(float(box.attrib['xbr']))
+    xtl = int(float(box.attrib['xtl']))
+    ybr = int(float(box.attrib['ybr']))
+    ytl = int(float(box.attrib['ytl']))
+    c_img = img[ytl:ybr, xtl:xbr]
     
     return c_img
 
-def make_obj(img, f_num, p_num, annotations, appearances, all_person):
 
-    for annotation in annotations:          # each person
-        for box in annotation:              # each frame
+def move(box):
+    action = box[0][4].text
+    if action == 'standing':
+        return 0
+    elif action == 'walking':
+        return 1
+
+
+
+
+def direction(box):
+
+    if int(box.attrib['pose_left']) == 1:
+        return 0
+    elif int(box.attrib['pose_right']) == 1:
+        return 1
+    elif int(box.attrib['pose_front']) == 1:
+        return 2
+    elif int(box.attrib['pose_back']) == 1:
+        return 3
+
+
+def look(box):
+    looking = box[0][2].text
+    if looking == 'not-looking':
+        return 0
+    elif looking == 'looking':
+        return 1
+
+def phone(box):
+    if int(box.attrib['phone']) == 0:
+        return 0
+    elif int(box.attrib['phone']) == 1:
+        return 1
+
+
+
+def make_obj_list(f_num, p_num, ano_track, appearances):
+
+    p_id = ano_track[0][0].text # id of pedestrian
+
+    for track in appearances:
+        if track.attrib['id'] == p_id:
+            for box in track:
+                if box.attrib['frame'] == str(f_num):
+                    # add obj list
+                    obj_list = [ '{0:04}'.format(p_num),   #[image_num, frame_num, person_ID, move, direction, looking, phone]
+                                f_num,
+                                p_id,
+                                move(ano_track),
+                                direction(box),
+                                look(ano_track) ,
+                                phone(box) ,
+                                ]                    
+
+    return obj_list
+
+
+def make_obj(img, f_num, p_num, vid_name, annotations, appearances, all_person):
+
+    for track in annotations:          # each person
+        for box in track:              # each frame
             if box.attrib['frame'] == str(f_num):
-                cut_img = cut_img(img, f_num, box)
+                c_img = cut_img(img, f_num, box)
+                cv2.imwrite('../images/{}/{}.jpg'.format(vid_name, p_num), c_img)
+                
+                obj_list = make_obj_list(f_num, p_num, track, appearances)
+                # print(obj_list)
+                all_person.append(obj_list) #TODO
+
                 p_num += 1
                 break
             # img and other things write
             
-
-
-
     return all_person, p_num
 
 def start(vid_path, vid_name):
@@ -97,9 +159,8 @@ def start(vid_path, vid_name):
     [mnm] - str
     '''
     cap = cv2.VideoCapture(vid_path+'JAAD_clips/'+vid_name+'.mp4')
-    
+
     annotations = parsing_annotation(vid_path+"annotations/", vid_name+'.xml') 
-    
     # checking the existence of a person
     if not len(annotations):
         print("'{}' doesn't have person".format(vid_name))
@@ -109,23 +170,19 @@ def start(vid_path, vid_name):
 
     f_counter = -1    # frame start 0 
     p_counter = 0     # person nuber (img name)
-    all_person = [] # each person has [image_num, frame_num, person_ID, move, direction, looking ]
+    all_person = [] # each person has [image_num, frame_num, person_ID, move, direction, looking, phone ]
     while True: # while for one frame
         f_counter += 1
         ret, img = cap.read()
 
-        if ret == False:
-            continue
+        if ret == True:            
+            all_person, p_counter = make_obj(img, f_counter, p_counter, vid_name, annotations, appearances, all_person)
         
-        all_person, p_counter = make_obj(img, f_counter, p_counter, annotations, appearances, all_person)
+        else:
+            break
 
-        # if cv2.waitKey(1)&0xFF == 27:
-        #     break
-        break
-
-    cap.release()
-    # cv2.destroyAllWindows()
-
+    with open('../labels/{}.pickle'.format(vid_name), 'wb') as f:
+        pickle.dump(all_person, f, pickle.HIGHEST_PROTOCOL)
 
 
 
